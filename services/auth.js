24e41aaee2ebf8626029167cd0638c2ac14b5e38
webcat68 <webcat682@gmail.com>
2016-11-28 06:13:55 -0500

    Add files via upload

--- services/auth.js
+++ services/auth.js
@@ -8,7 +8,7 @@
 		var REDIRECT_URI = '';
 
 		if (location.host == 'localhost:8000') {
-			CLIENT_ID =	'ffea365dd442e0bea569ee80424d32';
+			CLIENT_ID =	'fffea365dd442e0bea569ee80424d32';
 			REDIRECT_URI = 'http://localhost:8000/callback.html';
 		} else {
 			CLIENT_ID = '79a1aefd4b814068ad459369053d8151';
